1bdaa648357f3f4bf51e03c504014886d4b9a398
Liam <43959073+LiamBird@users.noreply.github.com>
2023-10-30 11:32:14 +0000

    Update BiologicData.py
    Added docstring

--- GalvPyle/BiologicData.py
+++ GalvPyle/BiologicData.py
@@ -4,7 +4,64 @@ class _CycleData(object):
             self.__dict__.update([(keys, [])])
 
 class BiologicGalv(object):
-    def __init__(self, filename, mass=None, rate_change=False, resume_label = " ", truncate_at=None, scale_mass=False):
+    """
+    Class for loading and displaying Galvanostatic data from Biologic cyclers
+    Requires mpt_to_df.py in same directory
+    
+    Inputs
+    ----------
+    filename (str):
+        The .mpt file (including its path)
+        
+    mass (float OR "ignore") (optional, default=None):
+        Mass of active material in g (if entering in mg, set scale_mass to True)
+        If mass == None, mass is read from header of Biologic .mpt file (if available, otherwise set to 1)
+        If mass == "ignore", mass is set to 1
+        Where mass is not set (i.e. mass defaults to '1'), graph outputs will be shown in mAh
+        Where mass is set, graph outputs will be shown in mAh/g
+        
+    theoretical_capacity (float) (default=1675):
+        The theoretical capacity of the cell in mAh/g
+        
+    rate_change (bool) (optional, default=False):
+        If multiple CRates are present in the cycling (e.g. 0.1, 0.2, 0.5...), splits the data into different CRates and enables attributes for labels in plotting
+    
+    resume_label (str) (optional, default = " "):
+        For use with rate change data with repeated CRate, e.g. (0.1, 0.2, 0.5, 0.1). Resume label will be appended to repeated CRates for unique dictionary keys
+        
+    truncate_at (int) (optional, default=None):
+        Last cycle to include in the data (e.g. to avoid final failed cycle affecting capacities)
+        
+    scale_mass (bool) (optional, default=False):
+        mass argument entered in g by default. To scale to mg, enter scale_mass=True
+        
+        
+    Attributes
+    ----------
+    n_cycles (int): 
+        Number of cycles for which discharge and charge data is available
+        
+    mass (float):
+        Mass (in g) used to calculate gravimetric capacity 
+    
+    charge AND discharge (_CycleData objects):
+        ┣ capacity: array of arrays of capacity data (one array per cycle)
+        ┃           units: mAh/g (if mass set) or mAh (if mass not set)
+        ┣ voltage: array of arrays of voltage data (one array per cycle)
+        ┣ time: array of arrays of times for data collection after onset of cycling (cumulative)
+        ┃           units: seconds (cumulative)
+        └ summary_capacity: final capacity at the end of the cycle
+                            units: mAh/g (if mass set) or mAh (if mass not set)
+                            
+    Methods
+    ----------
+    plot_capacities(y_max=1675):
+        Plots the summary_capacity data as a function of cycle number
+        
+    plot_capacity_voltage(cycles_to_plot="all", formation_cycles=2)
+        Plots the discharge/ charge curves for the specified cycles
+    """
+    def __init__(self, filename, mass=None, theoretical_capacity=1675, rate_change=False, resume_label = " ", truncate_at=None, scale_mass=False):
         from mpt_to_df import mpt_to_df
         import matplotlib.pyplot as plt
         import numpy as np
@@ -33,9 +90,9 @@ class BiologicGalv(object):
                         if "Battery capacity" in line:
                             capacity_read = float(re.findall("\d+.\d+", line)[0])
                             if scale_mass == False:
-                                self.mass = capacity_read/1675
+                                self.mass = capacity_read/theoretical_capacity
                             else:
-                                self.mass = capacity_read/1.675
+                                self.mass = capacity_read/(theoretical_capacity/1000)
                             self._mass_set = True
                 if self._mass_set == False:
                     self.mass = 1
@@ -142,7 +199,24 @@ class BiologicGalv(object):
             self.CRates_cycles = dict([(CRates_ordered[n], int(float(n_repeats[n]))) for n in range(len(n_repeats))])
             self.CRate_change_cycles = [int(np.sum([*self.CRates_cycles.values()][:n])+n) for n in range(len(self.CRates_cycles)+1)]
             
-    def plot_capacities(self, y_max=1675):#
+    def plot_capacities(self, y_max=1675):
+        """
+        Plots the discharge and charge capacities as a function of cycle number
+        
+        Inputs
+        ----------
+        y_max (float) (optional, default=1675):
+            the upper y limit of the plot
+            
+        Returns
+        ----------
+        fig, ax (figure, subplots):
+            Enables control of properties after plotting
+            Example:
+            f, ax = self.plot_capacities()
+            ax.legend(loc="upper left")
+            
+        """
         import matplotlib.pyplot as plt
         import numpy as np
         f, ax = plt.subplots()
@@ -158,6 +232,27 @@ class BiologicGalv(object):
         return f, ax
             
     def plot_capacity_voltage(self, cycles_to_plot="all", formation_cycles=2):
+        """
+        Plots charge/ discharge curves for specified cycles, differentiated by colour
+
+        Inputs
+        ----------
+        cycles_to_plot : (str or list of integers) (optional, default="all")
+            If "all", plots all the available data
+            If list of integers (e.g. [0, 1, 10, 20, 30...]) plots the specified cycles if available
+        formation_cycles : (int) (optional, default=2)
+            If slow formation cycles are used, shows these curves as dotted lines
+            Set to 0 if no formation cycles are used. 
+
+        Returns
+        ----------
+        fig, ax (figure, subplots):
+            Enables control of properties after plotting
+            Example:
+            f, ax = self.plot_capacities()
+            ax.legend(loc="upper left")
+
+        """
         import matplotlib.pyplot as plt
         import numpy as np
         f, ax = plt.subplots()
